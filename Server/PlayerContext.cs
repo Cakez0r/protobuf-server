52bd1a1d3d9b4f83fd9c608dc4c976edb88a6c56
Cakez0r <lewis.ellington@gmail.com>
2012-11-23 07:34:17 +0000

    First commit

---
+++ Server/PlayerContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class PlayerContext : NetPeer
+    {
+        public PlayerContext(Socket socket) : base(socket)
+        {
+
+        }
+
+        protected override void DispatchPacket(object packet)
+        {
+            Send(packet);
+        }
+
+        public void Update(TimeSpan dt)
+        {
+
+        }
+    }
+}
