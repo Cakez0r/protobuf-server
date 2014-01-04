cb19679a95cd7712aaf5805ab8eed896a32ff991
Lewis Ellington <lewis.ellington@tinderstone.com>
2014-01-04 02:49:57 +0000

    Add pool

---
+++ Server/Server/Utility/Pool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Utility
+{
+
+    public class Pool<T> where T : class, new()
+    {
+        private ConcurrentBag<T> m_pool = new ConcurrentBag<T>();
+
+        public T Take()
+        {
+            T obj = default(T);
+            m_pool.TryTake(out obj);
+
+            return obj ?? new T();
+        }
+
+        public void Return(T obj)
+        {
+            m_pool.Add(obj);
+        }
+    }
+}
